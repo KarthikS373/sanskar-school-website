0d462c4bda1fa150a1f1a8195b5ad950decbf1ca
KarthikS373 <lit2021012@iiitl.ac.in>
2023-02-25 19:07:55 +0530

    project initialization

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 60 * 60 * 24,
+    deviceSizes: [320, 640, 660, 768, 1024, 1600],
+  },
+}
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: false,
+})
+
+module.exports = withBundleAnalyzer(nextConfig)
